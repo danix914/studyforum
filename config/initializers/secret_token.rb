69e8bae18830396fb4e4b074045851b03aaab115
Det <danix914@gmail.com>
2011-05-09 14:49:30 +0800

    Study Forum init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studyforum::Application.config.secret_token = '453c13c682086f9580cc4a9206dbab47fcd47624819b94869a9069f30cea671a3c5be8769b9d9c17e33b4cfed549cf5053072e0b118d52beee05308577ce2075'
