84f0a0063b9a4e5f64232867bf091b5f57bfec06
Det <danix914@gmail.com>
2011-05-11 19:27:43 +0800

    kaminari setup

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,7 @@ class PostsController < ApplicationController
   before_filter :login_required, :except => [:index, :show]
 
   def index
-    @posts = @forum.posts.all
-    @a_page = Post.order(:name).page params[:page]
+    @posts = @forum.posts.order(:id).page(params[:page]).per(3)
   end
 
   def show
