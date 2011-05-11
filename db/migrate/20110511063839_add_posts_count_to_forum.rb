945bf134d2fed9c213b658b13de305bb43d0ac7a
Det <danix914@gmail.com>
2011-05-11 14:57:19 +0800

    modify migrate

--- db/migrate/20110511063839_add_posts_count_to_forum.rb
+++ db/migrate/20110511063839_add_posts_count_to_forum.rb
@@ -1,7 +1,14 @@
 class AddPostsCountToForum < ActiveRecord::Migration
   def self.up
+    add_column :formus, :posts_count, :integer, :default => 0
+
+    Forum.reset_column_information
+    Forum.find(:all).each do |p|
+      Forum.update_counter p.id, :posts_count => p.posts.length
+    end
   end
 
   def self.down
+    remove_column :forums, :posts_count
   end
 end
