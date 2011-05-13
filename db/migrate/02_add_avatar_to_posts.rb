1eab1499433282be78b0b3d9e76217c5d556d246
Det <danix914@gmail.com>
2011-05-13 15:15:22 +0800

    Paperclip done simply

--- db/migrate/02_add_avatar_to_posts.rb
+++ db/migrate/02_add_avatar_to_posts.rb
@@ -1,15 +1,15 @@
 class AddAvatarToPosts < ActiveRecord::Migration
   def self.up
-    add_column :Posts, :avatar_file_name,     :string
-    add_column :Posts, :avatar_content_type,  :string
-    add_column :Posts, :avatar_file_size,     :integer
-    add_column :Posts, :avatar_updated_at,    :datetime
+    add_column :posts, :avatar_file_name,     :string
+    add_column :posts, :avatar_content_type,  :string
+    add_column :posts, :avatar_file_size,     :integer
+    add_column :posts, :avatar_updated_at,    :datetime
   end
 
   def self.down
-    remove_column :Posts, :avatar_file_name
-    remove_column :Posts, :avatar_content_type
-    remove_column :Posts, :avatar_file_size
-    remove_column :Posts, :avatar_updated_at
+    remove_column :posts, :avatar_file_name
+    remove_column :posts, :avatar_content_type
+    remove_column :posts, :avatar_file_size
+    remove_column :posts, :avatar_updated_at
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,10 @@ ActiveRecord::Schema.define(:version => 20110510123625) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", :force => true do |t|
